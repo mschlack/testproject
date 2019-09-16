6e5f1640f43cfb705d87d3940f9ce4d106da9fce
michelleschlacks <michelle.schlacks@equipmentshare.com>
2019-09-16 14:33:53 -0500

    fixed css selector

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 var test;
 function getData() {
-    var form = document.getElementById("send_message")
+    var form = document.getElementById("submit_form")
     console.log(form);
     var formdata = new FormData(form);
     test = formdata;
